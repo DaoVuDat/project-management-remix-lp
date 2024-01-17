cc9cf620e037b6b858a4aeb65de3651fe14aacd4
Đào Vũ Đạt <dat.daovu@gmail.com>
2024-01-17 09:37:28 +0700

    fix font on safari

--- app/root.tsx
+++ app/root.tsx
@@ -12,11 +12,11 @@ import "plyr-react/plyr.css"
 import tailwindStylesheetUrl from "./styles/tailwind.css"
 
 export const links: LinksFunction = () => [
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
   {rel: "stylesheet", href: tailwindStylesheetUrl},
   {rel: "preconnect", href: "https://fonts.googleapis.com"},
   {rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous"},
   {rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Varela&display=swap"},
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
 
